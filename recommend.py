3a845fc91cbe253f6b39776326345d3cb84be81a
Nubula H <nubunubu68@gmail.com>
2026-07-24 17:30:54 +0530

    Add files via upload

--- recommend.py
+++ recommend.py
@@ -25,13 +25,13 @@ def get_recommendation(user_input):
         query_vector = tfidf.transform([user_input])
         cosine_sim1 = linear_kernel(query_vector, tfidf_matrix).flatten()
         if cosine_sim1.max()<0.1:
-             return None
+             return None, None
         similarity_scores = list(enumerate(cosine_sim1))
         similarity_scores = sorted(similarity_scores, key= lambda x: x[1], reverse = True)
         similarity_scores = similarity_scores[:10]
         similarity_index = [i[0] for i in similarity_scores]
         
-        return hadhith.iloc[similarity_index]
+        return hadhith.iloc[similarity_index],similarity_scores[:10]
 
 if st.button("Recommend"):
 
@@ -39,35 +39,30 @@ if st.button("Recommend"):
         st.warning("Please enter a topic")
 
     else:
-        recommendations = get_recommendation(user_input)
+        recommendations,similarity_scores = get_recommendation(user_input)
 
-    if recommendations is None:
-             st.error("❌ No relevant hadhith found. Please try another topic")
+        if recommendations is None:
+                st.error("❌ No relevant hadhith found. Please try another topic")
 
-    else:
-        st.subheader("Recommended Hadhiths")
+        else:
+            st.subheader("Recommended Hadhiths")
 
-    recommendations = get_recommendation(user_input)
     
-    if recommendations is None:
-         st.error("❌ No relevant hadhith found. Please try another topic")
-    
-    else:
-         st.subheader("ecommended Hadhiths")
+
+            for i in range(len(recommendations)):
+                st.write("### Recommendation", i+1)
+                st.write("Book", recommendations.iloc[i]["Book"])
+                st.write("Chapter", recommendations.iloc[i]["Chapter_Title_English"])
+                st.write("English")
+                st.write(recommendations.iloc[i]["English_Text"])
+                st.write("Arabic")
+                
+                st.write(recommendations.iloc[i]["Arabic_Text"])
+
+                st.markdown("---")
 
 
-    for i in range(len(recommendations)):
-        st.write("### Recommendation", i+1)
-        st.write("Book", recommendations.iloc[i]["Book"])
-        st.write("Chapter", recommendations.iloc[i]["Chapter_Title_English"])
-        st.write("English")
-    
-        st.write(recommendations.iloc[i]["English_Text"])
-        st.write("Arabic")
-        
-        st.write(recommendations.iloc[i]["Arabic_Text"])
-
-        st.markdown("---")
+#autofill
 
 
          
